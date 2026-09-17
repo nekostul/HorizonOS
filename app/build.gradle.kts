e9e0170c6e9677ce9b3d3a060e994f509fe2e0c9
nekostul <n3kostul@gmail.com>
2026-09-18 02:45:02 +0600

    bump version to 0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 37
         versionCode = 1
-        versionName = "0.5"
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
